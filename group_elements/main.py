33cbfd4d2386bbbfae9fb7e7595a99d590524d27
Mazhar Islam <mazharis@amazon.com>
2022-10-04 16:26:42 -0700

    Performance test using removal vs non removal of list deque

--- group_elements/main.py
+++ group_elements/main.py
@@ -1,9 +1,48 @@
+import copy
+import random
+import string
 from collections import deque
+import datetime
 
-# Press the green button in the gutter to run the script.
+
+def removal(group):
+    a = datetime.datetime.now()
+    output = []
+    while group:
+        for dq in group[:]:
+            if dq:
+                output.append(dq.popleft())
+            else:
+                group.remove(dq)
+    b = datetime.datetime.now()
+    print((b - a).microseconds)
+    # print(output)
+
+
+def non_removal(group):
+    a = datetime.datetime.now()
+    output = []
+    input_len = len(input_list)
+    while input_len > 0:
+        for dq in group:
+            if dq:
+                output.append(dq.popleft())
+                input_len -= 1
+    b = datetime.datetime.now()
+    print((b - a).microseconds)
+    # print(output)
+
+
 if __name__ == '__main__':
-    input_list = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'C1', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6']
+    # input_list = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'C1', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6']
+    INPUT_LEN = 1000000
     group = []
+    input_list = set()
+    for _ in range(INPUT_LEN):
+        input_list.add(random.choice(string.ascii_uppercase) + str(random.randint(0, 10000)))
+    input_list = list(input_list)
+    input_list.sort()
+
     i = 0
     unique_group_identifier = input_list[i][0]
     for elem in input_list:
@@ -17,14 +56,5 @@ if __name__ == '__main__':
             group.append(dq)
         dq.append(elem)
 
-    output = []
-    while group:
-        inner_list = []
-        for dq in group[:]:
-            if dq:
-                inner_list.append(dq.popleft())
-            else:
-                group.remove(dq)
-        if inner_list:
-            output.append(inner_list)
-    print(output)  # [['A1', 'B1', 'C1', 'E1'], ['A2', 'B2', 'E2'], ['A3', 'B3', 'E3'], ['A4', 'E4'], ['E5'], ['E6']]
+    removal(copy.deepcopy(group))
+    non_removal(copy.deepcopy(group))
